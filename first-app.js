a14f06c4744950534b96e48ab0e42889e940b290
Taher Ali. Attar <taher@talentica-all.com>
2018-09-27 16:11:19 +0530

    Assignment83

---
+++ first-app.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+
+
+if (Meteor.isClient) {
+Session.setDefault('counter',0);
+
+Template.hello.helpers({
+    counter: function () {
+        return Session.get('counter'); 
+    }
+});
+
+Template.hello.events({
+    'click button': function () {
+        Session.set('counter', Session.get('counter') + 1);
+    }
+});
+}
+
+if(Meteor.isServer) {
+    Meteor.startup(function () {
+
+    });
+}
